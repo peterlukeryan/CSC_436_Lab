7cbb50276eb5da594f19cdb06d48b4be74aaeba2
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-01-12 20:04:17 -0600

    Implement launcher icons

--- app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -35,11 +37,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.grad_cap),
+        contentDescription = "Graduation cap",
+        alpha = 0.3f
+    )
     Text(
         text = stringResource(R.string.graduation_announcement),
         fontSize = 50.sp,
         lineHeight = 50.sp,
+        textAlign = TextAlign.Center,
+        color = Color.Red,
         modifier = modifier
+
     )
 }
 
